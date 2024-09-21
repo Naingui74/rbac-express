66b1d9da2ae11e474d4f3ac60be3c5dabd29d2ed
Naingui <aaron.groux@epitech.eu>
2024-09-21 14:58:13 -0600

    Add Swagger Documentation

---
+++ swagger.js
@@ -0,0 +1,34 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API de RBAC',
+            version: '1.0.0',
+            description: 'Documentation de l\'API pour le projet RBAC',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
+            },
+        },
+    },
+    apis: ['./routes/*.js'], // Chemin vers les fichiers contenant les annotations Swagger
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+module.exports = (app) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+};
